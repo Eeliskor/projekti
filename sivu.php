776b3ea518df5f39588de202c0586b3d938bf607
Eelis Korhonen <eelis.korhonen@edu.savonia.fi>
2019-12-18 12:27:53 +0200

    Lisäsin toimivan databaseen liittämisen

--- sivu.php
+++ sivu.php
@@ -11,8 +11,24 @@
 <h1>My first PHP page</h1>
 
 <?php
+
+$mysqli = new mysqli("localhost", "root", "", "tyoajat");
+
+?>
+
+<?php
+
 echo "Hello World!";
-?> 
+
+?>
+
+<form action="sivu2.php" method="post">
+Anna projektin nimi: <input type="text" name="pronimi" /><br><br>
+Anna työntekijän nimi: <input type="text" name="tyonimi" /><br><br>
+Anna pvm milloin työ tehty: <input type="text" name="pvm" /><br><br>
+Anna työtunnit (h): <input type="text" name="tunnit" /><br><br>
+<input type="submit" />
+</form>
 
 </body>
 </html>
---
+++ sivu2.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+<?php
+
+//$mysqli = new mysqli("localhost", "root", "", "tyoajat");
+
+$con = new mysqli("localhost","root","", "tyoajat");
+
+if (!$con)
+
+  {
+    die('Could not connect: ' . mysql_error());
+  }
+
+$sql = ("INSERT INTO projekti (projekti_nimi, tyontekija, pvm, tunnit) VALUES ('$_POST[pronimi]','$_POST[tyonimi]','$_POST[pvm]','$_POST[tunnit]')");
+
+$stmt = $con->prepare($sql);
+$stmt->execute();
+
+echo "1 record added";
+?>
+</body>
+</html>
